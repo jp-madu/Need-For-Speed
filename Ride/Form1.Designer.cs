d1d87644465f4b1fb5a460a69a962370f2274f90
madu <jaypee1240@gmail.com>
2023-07-11 15:42:02 -1200

    Add project files.

---
+++ Ride/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace Ride
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Ride4 = new System.Windows.Forms.PictureBox();
+            this.Ride1 = new System.Windows.Forms.PictureBox();
+            this.Ride2 = new System.Windows.Forms.PictureBox();
+            this.Ride3 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.btnGear1 = new System.Windows.Forms.Button();
+            this.btnGear4 = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnBreak = new System.Windows.Forms.Button();
+            this.btnGear2 = new System.Windows.Forms.Button();
+            this.btnGear3 = new System.Windows.Forms.Button();
+            this.btnLaunch = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Ride4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ride1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ride2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ride3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ride4
+            // 
+            this.Ride4.Image = ((System.Drawing.Image)(resources.GetObject("Ride4.Image")));
+            this.Ride4.Location = new System.Drawing.Point(12, 12);
+            this.Ride4.Name = "Ride4";
+            this.Ride4.Size = new System.Drawing.Size(79, 83);
+            this.Ride4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Ride4.TabIndex = 0;
+            this.Ride4.TabStop = false;
+            this.Ride4.Click += new System.EventHandler(this.Ride4_Click);
+            // 
+            // Ride1
+            // 
+            this.Ride1.Image = ((System.Drawing.Image)(resources.GetObject("Ride1.Image")));
+            this.Ride1.Location = new System.Drawing.Point(12, 289);
+            this.Ride1.Name = "Ride1";
+            this.Ride1.Size = new System.Drawing.Size(100, 83);
+            this.Ride1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Ride1.TabIndex = 1;
+            this.Ride1.TabStop = false;
+            // 
+            // Ride2
+            // 
+            this.Ride2.Image = ((System.Drawing.Image)(resources.GetObject("Ride2.Image")));
+            this.Ride2.Location = new System.Drawing.Point(784, 289);
+            this.Ride2.Name = "Ride2";
+            this.Ride2.Size = new System.Drawing.Size(91, 83);
+            this.Ride2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Ride2.TabIndex = 2;
+            this.Ride2.TabStop = false;
+            // 
+            // Ride3
+            // 
+            this.Ride3.Image = ((System.Drawing.Image)(resources.GetObject("Ride3.Image")));
+            this.Ride3.Location = new System.Drawing.Point(763, 12);
+            this.Ride3.Name = "Ride3";
+            this.Ride3.Size = new System.Drawing.Size(100, 83);
+            this.Ride3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.Ride3.TabIndex = 3;
+            this.Ride3.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // btnGear1
+            // 
+            this.btnGear1.Location = new System.Drawing.Point(243, 415);
+            this.btnGear1.Name = "btnGear1";
+            this.btnGear1.Size = new System.Drawing.Size(75, 23);
+            this.btnGear1.TabIndex = 4;
+            this.btnGear1.Text = "Gear1";
+            this.btnGear1.UseVisualStyleBackColor = true;
+            this.btnGear1.Click += new System.EventHandler(this.btnGear1_Click);
+            // 
+            // btnGear4
+            // 
+            this.btnGear4.Location = new System.Drawing.Point(519, 415);
+            this.btnGear4.Name = "btnGear4";
+            this.btnGear4.Size = new System.Drawing.Size(75, 23);
+            this.btnGear4.TabIndex = 5;
+            this.btnGear4.Text = "Gear4";
+            this.btnGear4.UseVisualStyleBackColor = true;
+            this.btnGear4.Click += new System.EventHandler(this.btnGear4_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(55, 415);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(75, 23);
+            this.btnPlay.TabIndex = 6;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnBreak
+            // 
+            this.btnBreak.Location = new System.Drawing.Point(150, 415);
+            this.btnBreak.Name = "btnBreak";
+            this.btnBreak.Size = new System.Drawing.Size(75, 23);
+            this.btnBreak.TabIndex = 7;
+            this.btnBreak.Text = "Break";
+            this.btnBreak.UseVisualStyleBackColor = true;
+            this.btnBreak.Click += new System.EventHandler(this.btnBreak_Click);
+            // 
+            // btnGear2
+            // 
+            this.btnGear2.Location = new System.Drawing.Point(338, 415);
+            this.btnGear2.Name = "btnGear2";
+            this.btnGear2.Size = new System.Drawing.Size(75, 23);
+            this.btnGear2.TabIndex = 8;
+            this.btnGear2.Text = "Gear2";
+            this.btnGear2.UseVisualStyleBackColor = true;
+            this.btnGear2.Click += new System.EventHandler(this.btnGear2_Click);
+            // 
+            // btnGear3
+            // 
+            this.btnGear3.Location = new System.Drawing.Point(428, 415);
+            this.btnGear3.Name = "btnGear3";
+            this.btnGear3.Size = new System.Drawing.Size(75, 23);
+            this.btnGear3.TabIndex = 9;
+            this.btnGear3.Text = "Gear3";
+            this.btnGear3.UseVisualStyleBackColor = true;
+            this.btnGear3.Click += new System.EventHandler(this.btnGear3_Click);
+            // 
+            // btnLaunch
+            // 
+            this.btnLaunch.Location = new System.Drawing.Point(632, 415);
+            this.btnLaunch.Name = "btnLaunch";
+            this.btnLaunch.Size = new System.Drawing.Size(75, 23);
+            this.btnLaunch.TabIndex = 10;
+            this.btnLaunch.Text = "Launch";
+            this.btnLaunch.UseVisualStyleBackColor = true;
+            this.btnLaunch.Click += new System.EventHandler(this.btnLaunch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(875, 482);
+            this.Controls.Add(this.btnLaunch);
+            this.Controls.Add(this.btnGear3);
+            this.Controls.Add(this.btnGear2);
+            this.Controls.Add(this.btnBreak);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.btnGear4);
+            this.Controls.Add(this.btnGear1);
+            this.Controls.Add(this.Ride3);
+            this.Controls.Add(this.Ride2);
+            this.Controls.Add(this.Ride1);
+            this.Controls.Add(this.Ride4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Ride4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ride1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ride2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ride3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Ride4;
+        private System.Windows.Forms.PictureBox Ride1;
+        private System.Windows.Forms.PictureBox Ride2;
+        private System.Windows.Forms.PictureBox Ride3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Button btnGear1;
+        private System.Windows.Forms.Button btnGear4;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnBreak;
+        private System.Windows.Forms.Button btnGear2;
+        private System.Windows.Forms.Button btnGear3;
+        private System.Windows.Forms.Button btnLaunch;
+    }
+}
+
---
+++ Ride/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ride
+{
+    public partial class Form1 : Form
+    {
+        int Speed = 10;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Ride4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            Ride2.Visible = false;
+            Ride3.Visible = false;
+            Ride4.Visible = false;
+        }
+
+        private void btnBreak_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+        }
+
+        private void btnGear1_Click(object sender, EventArgs e)
+        {
+            Speed = 20;
+        }
+
+        private void btnGear2_Click(object sender, EventArgs e)
+        {
+            Speed = 30;
+        }
+
+        private void btnGear3_Click(object sender, EventArgs e)
+        {
+            Speed = 40;
+        }
+
+        private void btnGear4_Click(object sender, EventArgs e)
+        {
+            Speed = 50;
+        }
+
+        private void btnLaunch_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            timer2.Stop();
+            timer3.Stop();
+            timer4.Stop();
+            Ride1.Visible = true;
+            Ride2.Visible = false;
+            Ride3.Visible = false;
+            Ride4.Visible = false;
+            Ride1.Location = new Point(3, 328);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Ride1.Location = new Point(Ride1.Location.X + Speed, Ride1.Location.Y);
+
+            if (Ride1.Location.X > 784)
+            {
+                Ride1.Visible = false;
+                Ride2.Visible = true;
+                timer2.Start();
+                timer1.Stop();
+                Ride1.Location = new Point(3, 328);
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Ride2.Location = new Point(Ride2.Location.X, Ride2.Location.Y - Speed);
+
+            if (Ride2.Location.Y < 2)
+            {
+                Ride2.Visible = false;
+                Ride3.Visible = true;
+                timer3.Start();
+                timer2.Stop();
+                Ride2.Location = new Point(784, 328);
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            Ride3.Location = new Point(Ride3.Location.X - Speed, Ride3.Location.Y);
+
+            if (Ride3.Location.X < 4)
+            {
+                Ride3.Visible = false;
+                Ride4.Visible = true;
+                timer4.Start();
+                timer3.Stop();
+                Ride3.Location = new Point(784, 2);
+            }
+        }
+
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            Ride4.Location = new Point(Ride4.Location.X, Ride4.Location.Y + Speed);
+
+            if (Ride4.Location.Y > 328)
+            {
+                Ride4.Visible = false;
+                Ride1.Visible = true;
+                timer1.Start();
+                timer4.Stop();
+                Ride4.Location = new Point(4, 2);
+            }
+        }
+    }
+}
